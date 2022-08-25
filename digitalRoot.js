21fb4addd542321c997952bf7e9f692a947f06f7
Jonathan-Eboh <eboh1923@gmail.com>
2022-08-24 20:26:09 -0400

    Create digitalRoot.js

---
+++ digitalRoot.js
@@ -0,0 +1,22 @@
+/*
+
+Given n, take the sum of the digits of n. If that value has more than one digit, 
+continue reducing in this way until a single-digit number is produced. 
+The input will be a non-negative integer.
+
+*/
+
+
+function digitalRoot(n) {
+    intString = n.toString();
+    console.log(intString.length);
+
+    for (let i = 0; i < intString.length; i++) {
+        const element = intString[i];
+
+    }
+    //not done with this yet
+    return
+}
+
+console.log(digitalRoot(16));
